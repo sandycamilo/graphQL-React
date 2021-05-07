f7e7e3f3b67a9f0f4f47549b1dcfaf3c61d6e722
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-05-06 22:32:40 -0700

    mutations:creating links

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import './../styles/App.css';
 import React, { Component } from 'react';
-import LinkList from './LinkList';
+import CreateLink from './CreateLink';
+// import LinkList from './LinkList';
 
 class App extends Component {
   render() {
-    return <LinkList />;
+    return <CreateLink />;
   }
 }
 
