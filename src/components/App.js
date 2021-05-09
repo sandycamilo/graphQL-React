4c39106cce1530e6bb720c89a76af7ed192e46be
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-05-08 22:04:07 -0700

    filtering - searching the list of links

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import './../styles/App.css';
 import React from 'react';
 import CreateLink from './CreateLink';
 import Header from './Header';
-import LinkList from './LinkList';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Login'
+import Search from './Search';
 
 const App = () => {
  return (
@@ -12,13 +12,18 @@ const App = () => {
       <Header />
       <div className="ph3 pv1 background-gray">
         <Switch>
-          <Route exact path="/" component={LinkList} />
+          <Route
+            exact
+            path="/"
+            render={() => <Redirect to="/new/1" />}
+          />
           <Route
             exact
             path="/create"
             component={CreateLink}
           />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/search" component={Search} />
         </Switch>
       </div>
     </div>
